860a38ea48a7069ca6b25f096635c0d01cf35044
Adeel Ahmad <adeel@hashicorp.com>
2020-12-23 12:32:32 +0000

    amended latest version

--- outputs.tf
+++ outputs.tf
@@ -5,5 +5,5 @@ output "transit_key_keys" {
 
 output "transit_key_latest" {
   description = "Latest key version available."
-  value = vault_transit_secret_backend_key.default.latest
+  value = vault_transit_secret_backend_key.default.latest_version
 }
