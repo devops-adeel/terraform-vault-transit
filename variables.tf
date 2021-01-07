7b0756b91ab0a2d20c150d0c13829fe9c42a3688
Adeel Ahmad <adeel@hashicorp.com>
2021-01-07 19:15:33 +0000

    added standard policy with transit

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "service_name" {
   description = "The name of the service consuming the transit key"
-  type = string
+  type        = string
 }
 
 variable "env" {
   description = "The name of the environment which the key belongs to"
-  type = string
+  type        = string
 }
