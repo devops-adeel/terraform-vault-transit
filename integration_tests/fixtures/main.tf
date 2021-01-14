337e33acbab84e89b49c7c196732dec15bc9034f
Adeel Ahmad <adeel@hashicorp.com>
2021-01-14 16:51:05 +0000

    removed external approle module and using defaults

--- integration_tests/fixtures/main.tf
+++ integration_tests/fixtures/main.tf
@@ -16,16 +16,4 @@ module "transit" {
   providers = {
     vault = vault.default
   }
-  /* decrypt_group_ids = [module.vault_approle.group_id] */
-  /* encrypt_group_ids = [module.vault_approle.group_id] */
-}
-
-module "vault_approle" {
-  source = "git::https://devops-adeel@github.com/devops-adeel/terraform-vault-approle.git?ref=v0.4.1"
-  providers = {
-    vault = vault.default
-  }
-  application_name = local.application_name
-  env              = "dev"
-  service          = "web"
 }
