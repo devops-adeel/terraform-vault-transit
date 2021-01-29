648526f98620504711d9f992fc4b7f075044845a
Adeel Ahmad <8452076+devops-adeel@users.noreply.github.com>
2021-01-29 12:01:39 +0000

    feat: enabled entity as members (#6)
    Co-authored-by: Adeel Ahmad <adeel@hashicorp.com>

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
-variable "encrypt_group_ids" {
-  description = "List of Vault Identity Group IDs to be a member of Vault Identity Group for encrypting Transit keys"
+variable "encrypt_entity_ids" {
+  description = "List of Vault Identity IDs to be a member of Vault Identity Group for encrypting Transit keys"
   type        = list(any)
   default     = []
 }
 
-variable "decrypt_group_ids" {
-  description = "List of Vault Identity Group IDs to be a member of Vault Identity Group for decrypting Transit keys"
+variable "decrypt_entity_ids" {
+  description = "List of Vault Identity IDs to be a member of Vault Identity Group for decrypting Transit keys"
   type        = list(any)
   default     = []
 }
